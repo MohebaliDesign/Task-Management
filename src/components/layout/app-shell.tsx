e47b2beca7602060506735a953b75ff38bac9a6b
Mohamad <mohebali.uiux@gmail.com>
2026-09-09 13:48:11 +0330

    fixing bg color

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -43,7 +43,7 @@ export function AppShell({ children }: { children: ReactNode }) {
               </div>
             </div>
           </header>
-          <main className="flex-1 bg-background-subtle px-4 py-6 lg:px-8 lg:py-8">
+          <main className="flex-1 background px-4 py-6 lg:px-8 lg:py-8">
             <div className="mx-auto w-full max-w-6xl">{children}</div>
           </main>
         </div>
